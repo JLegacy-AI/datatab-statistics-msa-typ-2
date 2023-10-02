ef38417256c127bbd25f6f2abec4339ca0505110
Mr. J <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-10-03 00:58:11 +0500

    Minor Changes

--- src/components/tables/VarianceComponentTable.jsx
+++ src/components/tables/VarianceComponentTable.jsx
@@ -21,7 +21,7 @@ const VarianceComponentTable = ({ data, selectedColumns, LSL, USL }) => {
   return (
     <div>
       <div className="grid grid-cols-1">
-        <h3 className="text-xl font-semibold bg-black text-white  text-center">
+        <h3 className="text-xl min-w-[400px] max-w-[600px] font-semibold bg-black text-white  text-center">
           Variance Components
         </h3>
         <div className="grid grid-cols-1 gap-4">
@@ -90,134 +90,135 @@ const VarianceComponentTable = ({ data, selectedColumns, LSL, USL }) => {
               </tr>
             </tbody>
           </table>
-          <div className="overflow-x-scroll">
-            <table className="min-w-[700px] ">
-              <thead className="border-b-2 border-b-gray-300">
-                <tr className="w-full py-2.5 ">
-                  <td>Source</td>
-                  <td>StdDev</td>
-                  <td>Study Var.</td>
-                  <td>% Study Var.</td>
-                  <td>%Tolerance</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Total Gage R&R</td>
+        </div>
+        <h3 className="text-xl font-semibold bg-black text-white  text-center">
+          Gage Evalutation
+        </h3>
+        <div className="overflow-x-scroll">
+          <table className="min-w-[700px] ">
+            <thead className="border-b-2 border-b-gray-300">
+              <tr className="w-full py-2.5 ">
+                <td>Source</td>
+                <td>StdDev</td>
+                <td>Study Var.</td>
+                <td>% Study Var.</td>
+                <td>%Tolerance</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Total Gage R&R</td>
 
-                  <td>{numberFormat(result["Total Gage R&R"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(
-                      result["Total Gage R&R"]["Study Variance (6xSD)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Total Gage R&R"]["% Study Variance (%SV)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Total Gage R&R"]["% Tolerance (SV/Tol)"]
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Repeatability</td>{" "}
-                  <td>{numberFormat(result["Repeatability"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(
-                      result["Repeatability"]["Study Variance (6xSD)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Repeatability"]["% Study Variance (%SV)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Repeatability"]["% Tolerance (SV/Tol)"]
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Reproducibility</td>
+                <td>{numberFormat(result["Total Gage R&R"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(
+                    result["Total Gage R&R"]["Study Variance (6xSD)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Total Gage R&R"]["% Study Variance (%SV)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Total Gage R&R"]["% Tolerance (SV/Tol)"]
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Repeatability</td>{" "}
+                <td>{numberFormat(result["Repeatability"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(
+                    result["Repeatability"]["Study Variance (6xSD)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Repeatability"]["% Study Variance (%SV)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Repeatability"]["% Tolerance (SV/Tol)"]
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Reproducibility</td>
 
-                  <td>{numberFormat(result["Reproducibility"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(
-                      result["Reproducibility"]["Study Variance (6xSD)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Reproducibility"]["% Study Variance (%SV)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Reproducibility"]["% Tolerance (SV/Tol)"]
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Operator</td>
+                <td>{numberFormat(result["Reproducibility"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(
+                    result["Reproducibility"]["Study Variance (6xSD)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Reproducibility"]["% Study Variance (%SV)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Reproducibility"]["% Tolerance (SV/Tol)"]
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Operator</td>
 
-                  <td>{numberFormat(result["Operator"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(result["Operator"]["Study Variance (6xSD)"])}
-                  </td>
-                  <td>
-                    {numberFormat(result["Operator"]["% Study Variance (%SV)"])}
-                  </td>
-                  <td>
-                    {numberFormat(result["Operator"]["% Tolerance (SV/Tol)"])}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Part to Part</td>
+                <td>{numberFormat(result["Operator"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(result["Operator"]["Study Variance (6xSD)"])}
+                </td>
+                <td>
+                  {numberFormat(result["Operator"]["% Study Variance (%SV)"])}
+                </td>
+                <td>
+                  {numberFormat(result["Operator"]["% Tolerance (SV/Tol)"])}
+                </td>
+              </tr>
+              <tr>
+                <td>Part to Part</td>
 
-                  <td>{numberFormat(result["Part to Part"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(
-                      result["Part to Part"]["Study Variance (6xSD)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Part to Part"]["% Study Variance (%SV)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Part to Part"]["% Tolerance (SV/Tol)"]
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Total Variation</td>
+                <td>{numberFormat(result["Part to Part"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(
+                    result["Part to Part"]["Study Variance (6xSD)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Part to Part"]["% Study Variance (%SV)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(result["Part to Part"]["% Tolerance (SV/Tol)"])}
+                </td>
+              </tr>
+              <tr>
+                <td>Total Variation</td>
 
-                  <td>{numberFormat(result["Total Variation"]["stdDev"])}</td>
-                  <td>
-                    {numberFormat(
-                      result["Total Variation"]["Study Variance (6xSD)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Total Variation"]["% Study Variance (%SV)"]
-                    )}
-                  </td>
-                  <td>
-                    {numberFormat(
-                      result["Total Variation"]["% Tolerance (SV/Tol)"]
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+                <td>{numberFormat(result["Total Variation"]["stdDev"])}</td>
+                <td>
+                  {numberFormat(
+                    result["Total Variation"]["Study Variance (6xSD)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Total Variation"]["% Study Variance (%SV)"]
+                  )}
+                </td>
+                <td>
+                  {numberFormat(
+                    result["Total Variation"]["% Tolerance (SV/Tol)"]
+                  )}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
